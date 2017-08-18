31bd22ed41d8efc3fed4c5a183aef190c6a847c6
benhjt <benhjt@users.noreply.github.com>
2017-08-18 09:58:24 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+fetch('https://randomuser.me/api?results=250&nat=gb&inc=name,picture').then(response => {
+  return response.json();
+}).then(response => {
+  renderUsers(response.results);
+}).catch(error => {
+  console.error(error);
+});
+
+const io = new IntersectionObserver(entries => {
+  for (const entry of entries) {
+    if (entry.isIntersecting && !entry.target.src) {
+      entry.target.src = entry.target.getAttribute('data-src');
+      entry.target.onload = function () {
+        entry.target.removeAttribute('data-src');
+      };
+    }
+  }
+});
+
+const capitalize = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const renderUsers = (userList) => {
+  let userListMarkup = `<ul class="user-list">`;
+
+  for (let i = 0; i < userList.length; i++) {
+    let contact = userList[i];
+    userListMarkup += `
+      <li class="contact">
+        <img class="contact__picture" data-src="${contact.picture.thumbnail}">
+        <span class="contact__info">${capitalize(contact.name.first)} ${capitalize(contact.name.last)}</span>
+      </li>
+    `;
+  }
+
+  userListMarkup += `</ul>`;
+
+  document.querySelector('.container').innerHTML = userListMarkup;
+  document.querySelectorAll('.contact__picture').forEach(elem => io.observe(elem));
+};
